c57faceb1030aa4b9fdee14065da2733fbd653c9
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2022-12-19 22:09:39 +0100

    Added JS

---
+++ script.js
@@ -0,0 +1,21 @@
+var database =[
+    {
+        username: "Elly",
+        password: "supersecret"
+    },
+
+];
+
+var newFeed = [
+    {
+        username: "Bobby",
+        timeline: "So Tired from all that learning"
+    },
+    {
+        username: "Sally",
+        timeline: "Javaschript is so cool"
+    },
+];
+
+var userName = prompt("What is your username?");
+var Password = prompt("What is your password?");
